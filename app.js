d39f43d3402e33a2fdb4c7d238ff3065d7cc1b27
AndreasHolmAndersen <andreas.holm.andersen@gmail.com>
2022-03-02 15:14:23 +0100

    finished the skeleton for the mandatory

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
 const express = require("express");
 const app = express();
-const path = require("path");
+const fs = require("fs");
+
 app.use(express.static("public"));
 
+const nav = fs.readFileSync("./public/components/nav.html").toString();
+const footer = fs.readFileSync("./public/components/footer.html").toString();
+
+const home = fs.readFileSync("./public/pages/home.html").toString();
+const javascriptTheory = fs.readFileSync("./public/pages/javascript-theory.html").toString();
+const restApi = fs.readFileSync("./public/pages/rest-API.html").toString();
+const ssr = fs.readFileSync("./public/pages/SSR.html").toString();
+const tools = fs.readFileSync("./public/pages/tools.html").toString();
+
+const homePage = nav.replace("%%DOCUMENT_TITLE%%", "Home") + home + footer;
+const javascriptTheoryPage = nav.replace("%%DOCUMENT_TITLE%%", "Javascript Theory") + javascriptTheory + footer;
+const restApiPage = nav.replace("%%DOCUMENT_TITLE%%", "Rest API") + restApi + footer;
+const ssrPage = nav.replace("%%DOCUMENT_TITLE%%", "SSR") + ssr + footer;
+const toolsPage = nav.replace("%%DOCUMENT_TITLE%%", "Tools") + tools + footer;
+
+app.get("/", (req,res) => {
+  res.send(homePage);
+});
+
+app.get("/javascriptTheory", (req,res) => {
+  res.send(javascriptTheoryPage);
+});
+
+app.get("/restAPI", (req,res) => {
+  res.send(restApiPage);
+});
+
+app.get("/SSR", (req,res) => {
+  res.send(ssrPage);
+});
+
+app.get("/tools", (req,res) => {
+  res.send(toolsPage);
+});
 
 
 
